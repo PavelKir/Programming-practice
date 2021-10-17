993fa35cdbac4ec73c6eddd8e6d742a83190b0ee
PavelKir <ydaf02@gmail.com>
2021-10-18 02:53:39 +0500

    Added GameOver

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,54 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+public class PlayerStats
+{
+    int playerHP = 50;
+    int shotDamage = 10;
+    int jumpDamage = 10;
 
+    public int PlayerHP
+    {
+        get
+        {
+            return playerHP;
+        }
+    }
+    public int ShotDamage
+    {
+        get
+        {
+            return shotDamage;
+        }
+    }
+    public int JumpDamage
+    {
+        get
+        {
+            return jumpDamage;
+        }
+    }
+    public void AddHP(int amount)
+    {
+        playerHP += amount;
+    }
+    public void DamageReceived(int damage)
+    {
+        playerHP -= damage;
+        if (playerHP <= 0)
+        {
+            GameManager.Instance.GameOver();
+        }
+        Debug.Log(PlayerHP);
+    }
+
+}
 
 
 public class GameManager : MonoBehaviour
 {
-    public class PlayerStats
-    {
-        int playerHP = 50;
-        int shotDamage = 10;
-        int jumpDamage = 10;
+    
 
-        public int PlayerHP
-        {
-            get
-            {
-                return playerHP;
-            }
-        }
-        public int ShotDamage
-        {
-            get
-            {
-                return shotDamage;
-            }
-        }
-        public int JumpDamage
-        {
-            get
-            {
-                return jumpDamage;
-            }
-        }
-        public void AddHP(int amount)
-        {
-            playerHP += amount;
-        }
-        public void DamageReceived(int damage)
-        {
-            playerHP -= damage;
-            if (playerHP<=0)
-            {
-                // конец игры
-            }
-            Debug.Log(PlayerHP);
-        }
-        
-    }
+    public bool isGameOver = false;
     public static GameManager Instance;
     public PlayerStats playerStats;
     // Start is called before the first frame update
@@ -63,5 +66,9 @@ public class GameManager : MonoBehaviour
         playerStats = new PlayerStats();
     }
 
-
+    public void GameOver()
+    {
+        isGameOver = true;
+        SceneManager.LoadScene(0);
+    }
 }
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -63,6 +63,9 @@ public class PlayerManager : MonoBehaviour
         {
             isPlayerGrounded = true;
         }
-
+        if (collision.gameObject.CompareTag("Exit"))
+        {
+            GameManager.Instance.GameOver();
+        }
     }
 }
